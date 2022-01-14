35416879114fd443d7844cbd99f52e619ebeee12
Temple Okosun <okosun.temple@gmail.com>
2022-01-13 19:25:21 -0500

    added chalk & yargs packages

---
+++ notes.js
@@ -0,0 +1,12 @@
+const getNotes = () => {
+    return `Your notes...`
+}
+
+const addNote = (title, body) => {
+    return `Your notes...`
+}
+
+module.exports ={
+    getNotes: getNotes,
+    addNote: addNote
+}
